94b705f502ce799dc5aba64297b9e43152ca91b8
delafuentej <jfuentelucas@gmail.com>
2024-07-09 13:22:51 +0200

    init project: folder structure & navigation

---
+++ src/presentation/screens/pokemon/PokemonScreen.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable prettier/prettier */
+
+import { Text, View } from 'react-native';
+
+export const PokemonScreen = () => {
+  return (
+    <View>
+        <Text>PokemonScreen</Text>
+    </View>
+  );
+};
---
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/react-in-jsx-scope */
+
+import { Text, View } from 'react-native';
+
+export const SearchScreen = () => {
+  return (
+    <View>
+        <Text>SearchScreen</Text>
+    </View>
+  );
+};
