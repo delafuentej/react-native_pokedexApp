5d0f6e82b1c20e3c0a8c8926ee3221540fb7f961
delafuentej <jfuentelucas@gmail.com>
2024-07-11 20:06:09 +0200

    pokemonCard added

---
+++ src/config/theme/globalStyles.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+
+import { StyleSheet } from "react-native";
+
+
+
+
+export const globlaStyles = StyleSheet.create({
+    globalMargin: {
+        marginHorizontal: 20,
+    },
+});
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,18 @@
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable jsx-quotes */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable prettier/prettier */
 
-import { StyleSheet, View } from 'react-native';
+import {  StyleSheet, View } from 'react-native';
 import { Text , Button, ActivityIndicator } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
 import { useQuery } from '@tanstack/react-query';
 import { BgImg } from '../../components/ui/BgImg';
+import { Pokemon } from '../../../domain/entities/pokemon';
+import { FlatList } from 'react-native-gesture-handler';
+import { globlaStyles } from '../../../config/theme/globalStyles';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../../components/pokemons/PokemonCard';
 
 
 
@@ -15,16 +21,32 @@ import { BgImg } from '../../components/ui/BgImg';
 
 export const HomeScreen = () => {
 
+    const {top} = useSafeAreaInsets();
 
-  const {isLoading, data = []} = useQuery({
+  const {isLoading, data: pokemons = []} = useQuery({
     
       queryKey: ['pokemons'],
       queryFn: ()=> getPokemons(1),
       staleTime: 1000 * 60 * 60, //60 minutes
   });
   return (
-    <View style={styles.bgImgPosition}>
-      <BgImg />
+    <View  style={globlaStyles.globalMargin}>
+      <BgImg style={styles.bgImgPosition}/>
+
+      <FlatList 
+          data={pokemons}
+          keyExtractor={(pokemon : Pokemon, index) => `${pokemon.id}-${index}`}
+          numColumns={2}
+          ListHeaderComponent={()=>(
+            <Text variant="displayMedium">Pokedex</Text>
+          )}
+
+          style={{ paddingTop: top + 20}}
+          renderItem= {({item})=>(
+           <PokemonCard  pokemon={item} />
+        )}
+
+        />
    
     </View>
   );
