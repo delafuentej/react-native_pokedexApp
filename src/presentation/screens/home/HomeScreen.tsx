ea2aa7bda533e244394b551eecee200f6101fedb
delafuentej <jfuentelucas@gmail.com>
2024-07-13 11:28:40 +0200

    infinite scroll implemented

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -6,7 +6,7 @@
 import {  StyleSheet, View } from 'react-native';
 import { Text , Button, ActivityIndicator } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery} from '@tanstack/react-query';
 import { BgImg } from '../../components/ui/BgImg';
 import { Pokemon } from '../../../domain/entities/pokemon';
 import { FlatList } from 'react-native-gesture-handler';
@@ -15,25 +15,50 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { PokemonCard } from '../../components/pokemons/PokemonCard';
 
 
-
-
-
-
 export const HomeScreen = () => {
 
     const {top} = useSafeAreaInsets();
 
-  const {isLoading, data: pokemons = []} = useQuery({
-    
-      queryKey: ['pokemons'],
-      queryFn: ()=> getPokemons(1),
+    // basic form http request:
+  //   const {isLoading, data: pokemons = []} = useQuery({
+  //     queryKey: ['pokemons'],
+  //     queryFn: ()=> getPokemons(1),
+  //     staleTime: 1000 * 60 * 60, //60 minutes
+  // });
+
+
+// with infinitscroll:
+  const {isLoading, data, fetchNextPage} = useInfiniteQuery({
+      queryKey: ['pokemons', 'infinite'],
+      initialPageParam: 0,
+      queryFn: ( params )=> getPokemons(params.pageParam),
+      getNextPageParam: (lastPage, pages) => pages.length,
       staleTime: 1000 * 60 * 60, //60 minutes
   });
   return (
     <View  style={globlaStyles.globalMargin}>
       <BgImg style={styles.bgImgPosition}/>
 
+      {/*  with infinite scroll */}
       <FlatList 
+          data={data?.pages.flat() ?? []}
+          keyExtractor={(pokemon : Pokemon, index) => `${pokemon.id}-${index}`}
+          numColumns={2}
+          ListHeaderComponent={()=>(
+            <Text variant="displayMedium">Pokedex</Text>
+          )}
+
+          style={{ paddingTop: top + 20}}
+          onEndReachedThreshold={0.7}
+          onEndReached ={() => fetchNextPage()}
+          showsVerticalScrollIndicator= {false}
+          renderItem= {({item})=>(
+           <PokemonCard  pokemon={item} />
+        )}
+
+        />
+
+      {/* <FlatList 
           data={pokemons}
           keyExtractor={(pokemon : Pokemon, index) => `${pokemon.id}-${index}`}
           numColumns={2}
@@ -46,7 +71,7 @@ export const HomeScreen = () => {
            <PokemonCard  pokemon={item} />
         )}
 
-        />
+        /> */}
    
     </View>
   );
